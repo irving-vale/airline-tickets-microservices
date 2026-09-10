415d7079d7f948a0602de2bf0f95e6a9513694d4
Irving Valencia <75141924+irving-vale@users.noreply.github.com>
2026-09-10 16:37:05 -0600

    Add observability, tracing, and logging configurations to all microservices, update dependencies, migrate property files to YAML, and extend Docker and Eureka setup.

--- msvc-gateway/src/main/java/com/irving/cursoKubernetes/config/LoggingGlobalFilter.java
+++ msvc-gateway/src/main/java/com/irving/cursoKubernetes/config/LoggingGlobalFilter.java
@@ -5,9 +5,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
-
+@Component
 public class LoggingGlobalFilter implements GlobalFilter, Ordered {
 
     private static final Logger log = LoggerFactory.getLogger(LoggingGlobalFilter.class);
